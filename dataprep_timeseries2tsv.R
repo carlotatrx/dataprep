31d73028cd5a489b2136c4477d2e437d0f3d33f0
Corbella Alcantara <ccorbella@giub.local@climcal4.giub.unibe.ch>
2025-03-19 15:49:07 +0100

    tsvs

--- dataprep_timeseries2tsv.R
+++ dataprep_timeseries2tsv.R
@@ -1,7 +1,41 @@
 library(dataresqc)
+library(lubridate)
+
 source("/home/ccorbella/scratch2_symboliclink/code/KF_assimilation/dataprep/write_sef_f.R")
 
 outdir <- '/home/ccorbella/scratch2_symboliclink/files/station_timeseries_preprocessed/'
+
+
+# UPPSALA -----------------------------------------------------------------
+
+df <- read.csv('/home/ccorbella/scratch2_symboliclink/files/station_timeseries_preprocessed/Uppsala_PRMSL.csv')
+
+df.p.Upp <- data.frame(
+  Year= year(df$Date),
+  Month=month(df$Date),
+  Day=day(df$Date),
+  Hour='NA',
+  Minute='NA',
+  Value=df$PRMSL
+)
+
+meta <- list(
+  ID = "IMPROVE_Uppsala",
+  Name = "Uppsala",
+  Lat = 59.8605,
+  Lon = 17.6407,
+  Alt = 15,
+  Source = "PALEO-RA"
+)
+
+write_sef_f(Data=df.p.Upp, outfile="Uppsala_p.tsv",
+            outpath=outdir,
+            cod=meta[["ID"]],
+            variable="p",
+            nam=meta[["Name"]],
+            lat=meta[["Lat"]],
+            lon=meta[["Lon"]], alt=meta[["Alt"]], sou=meta[["Source"]], metaHead = "from Final_Series_Improve",
+            units="unknown", stat="point",keep_na = F)
 
 
 # VALÈNCIA ----------------------------------------------------------------
@@ -24,7 +58,7 @@ df.ta.Valencia <- data.frame(
   Month=df$Month,
   Day=df$Day,
   Hour=df$Hour,
-  Minute='0',
+  Minute='NA',
   Value=as.numeric(df$Term) * 1.25
 )
 
@@ -33,12 +67,12 @@ df.p.Valencia <- data.frame(
   Month=df$Month,
   Day=df$Day,
   Hour=df$Hour,
-  Minute='0',
+  Minute='NA',
   Value=(as.numeric(df$Bar.p.) * 23.22 + as.numeric(df$Bar.l.) * 1.935 ) * 1.3322
 )
 
 
-write_sef(Data=df.ta.Valencia,
+write_sef_f(Data=df.ta.Valencia,outfile="Valencia_ta_subdaily.tsv",
           outpath=outdir,
           cod=meta[["ID"]],
           variable="ta",
@@ -48,14 +82,14 @@ write_sef(Data=df.ta.Valencia,
           link=meta[["Link"]], units="C", stat="point",
           meta="orig_ta=Reaumur", keep_na = F)
 
-write_sef(Data=df.p.Valencia,
+write_sef_f(Data=df.p.Valencia,outfile="Valencia_p_subdaily.tsv",
           outpath=outdir,
           cod=meta[["ID"]],
           variable="p",
           nam=meta[["Name"]],
           lat=meta[["Lat"]],
           lon=meta[["Lon"]], alt=meta[["Alt"]], sou=meta[["Source"]], metaHead = "orig_p=Castillian_inch_&_lines",
-          link=meta[["Link"]], units="C", stat="point",
+          link=meta[["Link"]], units="unknown", stat="point",
           meta="orig_p=Castillian_inch_&_lines", keep_na = F)
 
 
@@ -67,8 +101,8 @@ df.ta.Yli <- data.frame(
   Year=df$Year,
   Month=df$Month,
   Day=df$Day,
-  Hour='9',
-  Minute='0',
+  Hour='NA',
+  Minute='NA',
   Value=df$TMP2m
 )
 
@@ -76,8 +110,8 @@ df.p.Yli <- data.frame(
   Year=df$Year,
   Month=df$Month,
   Day=df$Day,
-  Hour='9',
-  Minute='0',
+  Hour='NA',
+  Minute='NA',
   Value=df$PRMSL
 )
 
@@ -102,7 +136,7 @@ write_sef_f(Data=df.ta.Yli, outfile="Yli_ta.tsv",
           meta="orig_ta=C", keep_na = F
 )
 
-write_sef_f(Data=df.ta.Yli, outfile="Yli_p.tsv",
+write_sef_f(Data=df.p.Yli, outfile="Yli_p.tsv",
             outpath=outdir,
             cod=meta[["ID"]],
             variable="p",
