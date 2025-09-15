e9b6d9d6cc3e255b2c860d4e07ec56b59f0371da
carlotatrx <ccorbella56@gmail.com>
2025-09-15 15:49:26 +0200

    tuebingen, vienna

--- indiv_series/Tuebingen.R
+++ indiv_series/Tuebingen.R
@@ -1,6 +1,6 @@
 rm(list=ls())
 library(dataresqc)
-library(readxl)
+library(XLConnect)
 library(dplyr)
 library(tidyr)
 source('/home/ccorbella/scratch2_symboliclink/code/KF_assimilation/dataprep/helpfun.R')
@@ -9,22 +9,78 @@ name <- "Tuebingen"
 cod <- "Europe_Tuebingen_1"
 
 file <- paste0('/scratch3/PALAEO-RA/DataRescue/Projects/Europe/3_Revised/',name,'/Europe_T3_DE_',name,'_1691-1694_subdaily.xlsx')
-raw <- read_excel(file, sheet = 1, skip  = 6)
 
 lat	<- 48.5216364
 lon	<- 9.0576448
 alt	<- NA
 metaHead <- "Observer=Camerarius"
 
-# pressure conversion
-conversions <- list(p  = function(x, ta) round(convert_pressure(as.numeric(x), f = 27.07, lat = lat, alt = alt, atb=ta), 1))
+
+fill_val <- function(x){
+  
+  ind <- which(!is.na(x))
+  #ind <- switch((ind[length(ind)]==length(x))+1, c(ind),ind)
+  for(i in 1:(length(ind))){
+    if(i==length(ind)){
+      x[ind[i]:length(x)] <- x[ind[i]]
+    } else {
+      x[ind[i]:(ind[i+1]-1)] <- x[ind[i]]
+    }
+  }
+  return(x)
+}
+raw <- readWorksheetFromFile(file, sheet=1, startRow=8, header=FALSE, colTypes="character")
+
+
+
+meta_frac <- function(g, b1, b2) {
+  if (is.na(b1) | is.na(b2)) {
+    as.character(g)  # just return the integer part
+  } else {
+    paste0(g, ".", b1, "/", b2)
+  }
+}
+
+p_frac <- function(g, b1, b2) {
+  if (is.na(b1) | is.na(b2)) {
+    as.integer(g)  # just return the integer part
+  } else {
+    round(as.numeric(g) + as.numeric(b1)/as.numeric(b2)/10 ,2)
+  }
+}
+
+Year <- as.integer(fill_val(raw[,1]))
+Month <- as.integer(fill_val(raw[,2]))
+Day <- as.integer(fill_val(raw[,3]))
+hours <- as.integer(substr(raw[,4],12,13))
+minutes <- as.integer(substr(raw[,4],15,16))
+meta_time <- raw[,4]
+j <- which(nchar(meta_time)>1)
+meta_time[j] <- substr(meta_time[j], 12, 16)
+meta_time <- paste0("orig.time=", meta_time)
+p.raw <- as.integer(raw[,8])
+p.frac1 <- as.integer(raw[,9])
+p.frac2 <- as.integer(raw[,10])
+meta_p <- paste0('orig_p=', mapply(meta_frac, p.raw, p.frac1, p.frac2))
+p <- mapply(p_frac, p.raw, p.frac1, p.frac2)
+
 time.offset <- as.numeric(lon)*12/180
 
-# Keep only the first 10 columns and rename
-df <- raw %>%
-  select(c(1:4,7)) %>%
-  rename(
-    Year = 1, Month = 2, Day = 3, Hour = 4, p.zoll = 4, p.in = 5, ta.R = 6, dd.orig = 7
-  ) %>%
- ...
- # pressure units unknown
+## Write SEF files
+write_sef_f(data.frame(Year, Month, Day, hours, minutes,p),
+            outfile=paste0(name, "_p_subdaily.tsv"),
+            outpath="/home/ccorbella/scratch2_symboliclink/files/station_timeseries_preprocessed/",
+            variable = "p",
+            cod = cod,
+            nam = name,
+            lat = lat,
+            lon = lon,
+            alt = alt,
+            sou = "PALAEO-RA",
+            units = "unknown",
+            stat = "point",
+            meta = paste0(meta_time, " | ", meta_p),
+            metaHead = metaHead,
+            period = 0,
+            time_offset = time.offset,
+            keep_na = F)
--- indiv_series/Vienna.R
+++ indiv_series/Vienna.R
@@ -1,37 +1,220 @@
+rm(list=ls())
 library(dataresqc)
-library(XLConnect)
+library(readxl)
+library(tidyr)
+library(dplyr)
+source('/home/ccorbella/scratch2_symboliclink/code/KF_assimilation/dataprep/helpfun.R')
 
-#### functions for sef files...
-fill_val <- function(x){
-  
-  ind <- which(!is.na(x))
-  #ind <- switch((ind[length(ind)]==length(x))+1, c(ind),ind)
-  for(i in 1:(length(ind))){
-    if(i==length(ind)){
-      x[ind[i]:length(x)] <- x[ind[i]]
-    } else {
-      x[ind[i]:(ind[i+1]-1)] <- x[ind[i]]
-    }
-  }
-  return(x)
+name <- "Vienna"
+lat	<- 48.2081743
+lon	<- 16.3738189
+alt	<- NA
+metaHead <- "Observer=Beintema van Peima"
+
+file <- "/scratch3/PALAEO-RA/daily_data/original/Vienna/Europe_T3_AT_Vienna_1709-1715_subdaily.xls"
+raw <- read_excel(file, sheet=3, skip=6)
+
+get_date_range <- function(df) {
+  start.date <- paste0(df$Year[1],
+                       sprintf("%02d", df$Month[1]),
+                       sprintf("%02d", df$Day[1]))
+  n <- nrow(df)
+  end.date   <- paste0(df$Year[n],
+                       sprintf("%02d", df$Month[n]),
+                       sprintf("%02d", df$Day[n]))
+  paste0(start.date, "-", end.date)
 }
 
 
-files <- list.files("/scratch3/PALAEO-RA/daily_data/original/Vienna", full.names=T)
+# sheet number 3 ----------------------------------------------------------
 
-i <- 1
+# Keep only the first columns and rename
+df <- raw %>%
+  select(1:3, 6:9) %>%
+  rename(
+    Year = 1, Month = 2, Day = 3, ta.sign.morn = 4, ta.sign.eve = 6, ta.morn = 5, ta.eve = 7
+  ) %>%
+  # Fill down Year/Month/Day
+  mutate(across(where(is.character), ~ na_if(trimws(.x), ""))) %>%
+  fill(Year, Month, .direction = "down") %>%
+  mutate(
+    Year = as.integer(Year),
+    Month = as.integer(Month),
+    Day = as.integer(Day),
+    Minute = NA_integer_,
+    ta.sign.morn = as.character(ta.sign.morn),
+    ta.sign.eve  = as.character(ta.sign.eve)
+    
+  )
+head(df)
 
-template <- readWorksheetFromFile(files[i], sheet=ifelse(i==1,3,1), startRow=8, 
-                                  header=FALSE, colTypes="character")
+df.long <- df %>%
+  pivot_longer(
+    cols = c(ta.morn, ta.eve),
+    names_to = "tod",
+    names_prefix = "ta\\.",
+    values_to = "ta"
+  ) %>%
+  mutate(
+    ta = round(ta,1),
+    Hour = case_when(
+      tod=="morn" ~ 8L,
+      tod=="eve"  ~ 20L,
+      TRUE        ~ NA_integer_
+    ),
+    orig.hour = case_when(
+      tod=="morn" ~ "a.m.",
+      tod=="eve"  ~ "p.m.",
+      TRUE        ~ NA_character_
+    ),
+    ta.orig = case_when(
+      !is.na(ta.sign.morn) ~ paste0(ta.sign.morn, ta),
+      !is.na(ta.sign.eve)  ~ paste0(ta.sign.eve,  ta),
+      TRUE                 ~ as.character(ta)
+    ),
+    ta = case_when(
+      tod == "morn" & grepl("f", tolower(ta.sign.morn)) ~ -ta,
+      tod == "eve"  & grepl("f", tolower(ta.sign.eve))  ~ -ta,
+      TRUE ~ ta
+    ),
+    meta = paste0("orig.hour=", orig.hour, " | orig.ta=", ta.orig)
+  ) %>%
+  select(Year, Month, Day, Hour, Minute, Value = ta, meta) %>%
+  filter(!is.na(Value))
+head(df.long)
 
-Year <- as.integer(fill_val(template[,1]))
-Month <- as.integer(fill_val(template[,2]))
-Day <- fill_val(as.integer(template[,3]))
 
-if (i == 1) {
-  hours <- rep(NA, nrow(template))
-  minutes <- hours
-  meta_time <- ""
-}
+# sheet number 4 ----------------------------------------------------------
 
-head(template)
+raw2 <- read_excel(file, sheet=4, skip=6)
+
+df2 <- raw2 %>%
+  select(1:4, 7:10) %>%
+  rename(
+    Year = 1, Month = 2, Day = 3, orig.hour = 4, ta.sign.morn = 5, ta.sign.eve = 7, ta.morn = 6, ta.eve = 8
+  ) %>%
+  # Fill down Year/Month/Day
+  mutate(across(where(is.character), ~ na_if(trimws(.x), ""))) %>%
+  fill(Year, Month, Day, .direction = "down") %>%
+  mutate(
+    Year = as.integer(Year),
+    Month = as.integer(Month),
+    Day = as.integer(Day),
+    orig.hour = suppressWarnings(as.integer(orig.hour)),
+    Minute = NA_integer_,
+    ta.sign.morn = as.character(ta.sign.morn),
+    ta.sign.eve  = as.character(ta.sign.eve),
+    ta.morn = round(suppressWarnings(as.numeric(ta.morn)),1),
+    ta.eve  = round(suppressWarnings(as.numeric(ta.eve)), 1)
+  )
+head(df2)
+
+df2.long <- df2 %>%
+  pivot_longer(cols = c(ta.morn, ta.eve), names_to = "tod", values_to = "ta") %>%
+  mutate(
+    rounding = if_else(tod == "ta.morn", ta.sign.morn, ta.sign.eve),
+    Hour = case_when(
+      tod == "ta.morn" & orig.hour <= 12 ~ orig.hour,        # keep as-is
+      tod == "ta.eve"  & orig.hour < 12  ~ orig.hour + 12,    # convert to PM
+      tod == "ta.eve"  & orig.hour >= 12 ~ orig.hour,         # already PM
+      TRUE                          ~ NA_integer_
+    ),
+
+    meta.hour = if_else(tod=="ta.morn","g.a.","p.m."),
+    orig.ta = if_else(!is.na(rounding), paste0(rounding, ta), as.character(abs(ta))),
+    ta = case_when(
+      tod == "ta.morn" & grepl("f", tolower(ta.sign.morn)) ~ -ta,
+      tod == "ta.eve"  & grepl("f", tolower(ta.sign.eve))  ~ -ta,
+      TRUE ~ ta
+    ),
+    meta = paste0("orig.hour=", orig.hour, meta.hour, " | orig.ta=", orig.ta)
+  ) %>%
+  select(Year, Month, Day, Hour, Minute, Value = ta, meta) %>%
+  filter(!is.na(Value))
+
+head(df2.long)
+
+
+# sheet number 2 ----------------------------------------------------------
+
+raw3 <- read_excel(file, sheet=2, skip=6)
+
+df3 <- raw3 %>%
+  select(1:7) %>%
+  rename(
+    Year = 1, Month = 2, Day = 3, F.morn = 4, F.eve = 5, C.morn = 6, C.eve = 7
+  ) %>%
+  # Fill down Year/Month/Day
+  mutate(across(where(is.character), ~ na_if(trimws(.x), ""))) %>%
+  fill(Year, Month, .direction = "down") %>%
+  mutate(
+    
+    Year = as.integer(Year),
+    Month = as.integer(Month),
+    Day = as.integer(Day),
+    C.eve = as.numeric(C.eve),
+    ta.morn = case_when(
+      F.morn > 0 ~ -F.morn,
+      C.morn > 0 ~ C.morn,
+      TRUE ~ NA_real_
+    ),
+    ta.eve = case_when(
+      F.eve > 0 ~ -F.eve,
+      C.eve > 0 ~ C.eve,
+      TRUE ~ NA_real_
+    ),
+    ta.sign.morn = case_when(
+      F.morn > 0 ~ "F",
+      C.morn > 0 ~ "C",
+      TRUE ~ NA_character_
+    ),
+    ta.sign.eve = case_when(
+      F.eve > 0 ~ "F",
+      C.eve > 0 ~ "C",
+      TRUE ~ NA_character_
+    ),
+    
+    Minute = NA_integer_
+  )
+head(df3)
+
+df3.long <- df3 %>%
+  pivot_longer(cols = c(ta.morn, ta.eve), names_to = "tod", values_to = "ta") %>%
+  mutate(
+    rounding = if_else(tod == "ta.morn", ta.sign.morn, ta.sign.eve),
+    Hour = case_when(
+      tod == "ta.morn" ~ 8L,       
+      tod == "ta.eve"  ~ 20L,
+      TRUE             ~ NA_integer_
+    ),
+    
+    meta.hour = if_else(tod=="ta.morn","a.m.","p.m."),
+    orig.ta = if_else(!is.na(rounding), paste0(rounding, ".", abs(ta)), as.character(abs(ta))),
+
+    meta = paste0("orig.hour=", meta.hour, " | orig.ta=", orig.ta)
+  ) %>%
+  select(Year, Month, Day, Hour, Minute, Value = ta, meta) %>%
+  filter(!is.na(Value))
+
+head(df3.long)
+
+
+# combine and save --------------------------------------------------------
+
+df.all <- bind_rows(df.long, df2.long, df3.long) %>%
+  mutate(Value=round(Value, 1)) %>%
+  arrange(Year, Month, Day, Hour)
+
+# save
+write_sef_f(
+  Data=as.data.frame(df.all[, c("Year", "Month", "Day", "Hour", "Minute", "Value")]),
+  outfile=paste0("Vienna_",get_date_range(df.all), "_ta_subdaily.tsv"),
+  outpath="/home/ccorbella/scratch2_symboliclink/files/station_timeseries_preprocessed/",
+  cod="Europe_Vienna_1",
+  lat=lat, lon=lon, alt=alt,
+  variable="ta",
+  nam=name,
+  meta=df.all$meta,
+  metaHead=metaHead,
+  units="unknown", sou="PALAEO-RA", stat="point",keep_na = F,
+)
