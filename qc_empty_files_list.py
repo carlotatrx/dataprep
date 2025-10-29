61190be2f051b16b8310ba4f0a1169822f490bde
carlotatrx <ccorbella56@gmail.com>
2025-10-29 16:57:58 +0100

    list of empty qc files

---
+++ qc_empty_files_list.py
@@ -0,0 +1,24 @@
+import os
+
+path = "/scratch3/PALAEO-RA/daily_data/final/sef_tests/qc_tests/"
+
+empty_files = []
+nonempty_files = []
+
+for file in os.listdir(path):
+    if file.endswith(".txt"):
+        with open(os.path.join(path, file), "r") as f:
+            lines = f.readlines()
+            if len(lines) <= 1:
+                empty_files.append(file)
+            else:
+                nonempty_files.append(file)
+
+empty_files.sort()
+nonempty_files.sort()
+
+for file in empty_files:
+    print(f"Empty file: {file}")
+
+for file in nonempty_files:
+    print(f"Non-empty file: {file}")
